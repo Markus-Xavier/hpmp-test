15c355396b4eda0a126d0e5f2ae72d380c3c5b6e
DESKTOP-RVI3NBC\Xavier <mxrossio@gmail.com>
2020-11-29 09:01:34 -0600

    Adds better stairs for mans to walk up, adds jumping, fixes fast drop issue

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,9 +8,10 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f; 
 
     public Transform groundCheck;
-    public float groundDistance;
+    public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
     Vector3 velocity;
@@ -33,6 +34,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
